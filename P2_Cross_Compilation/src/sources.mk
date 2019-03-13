a304314ef3cbd34323e5cccb24b3f538620939a6
Joseph George <jge9@protonmail.ch>
2019-03-13 18:01:34 -0400

    finished up the makefile

--- P2_Cross_Compilation/src/sources.mk
+++ P2_Cross_Compilation/src/sources.mk
@@ -1,17 +1,16 @@
 #******************************************************************************
-# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+# Copyright (C) 2019 by Joseph George
 #
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are 
 # permitted to modify this and use it to learn about the field of embedded
-# software. Alex Fosdick and the University of Colorado are not liable for any
-# misuse of this material. 
+# software. Joseph George is not liable for any misuse of this material. 
 #
 #*****************************************************************************
 
-# Add your Source files to this variable
-SOURCES =
+SOURCES = main.c \
+			memory.c
 
-# Add your include paths to this variable
-INCLUDES = 
-
+INCLUDES = -I../include/msp432/ \
+		-I../include/common/ \
+		-I../include/CMSIS
